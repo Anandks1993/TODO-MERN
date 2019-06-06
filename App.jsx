b913a2cf0fb92c7c3d06f5291c6558918f36ad16
Anandks1993 <ksanand1993@gmail.com>
2019-06-06 15:23:58 +0530

    todo with mern

--- App.jsx
+++ App.jsx
@@ -1,34 +1,40 @@
-import React, { Component } from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CreateTodo from "./src/components/create-todo.component";
+import EditTodo from "./src/components/edit-todo.component";
+import TodosList from "./src/components/todo-list.component";
+import logo from "./src/assets/images/logo.png";
 
-import 'assets/common_styles/style.scss';
 
 class App extends Component {
-
-    state = {
-        message: 'Welcome to React JS Boilerplate!!!',
-    }
-
     render() {
         return (
-            <BrowserRouter>
-                <div style={{
-                    backgroundColor: '#000000',
-                    color: 'white',
-                    textAlign: 'center',
-                    height: '200px',
-                    fontSize: '50px',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}>
-                    <div>
-                        {this.state.message}
-                    </div>
+            <Router>
+                <div className="container">
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+                            <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+                        </a>
+                        <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+                        <div className="collpase navbar-collapse">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="navbar-item">
+                                    <Link to="/" className="nav-link">Todos</Link>
+                                </li>
+                                <li className="navbar-item">
+                                    <Link to="/create" className="nav-link">Create Todo</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <br />
+                    <Route path="/" exact component={TodosList} />
+                    <Route path="/edit/:id" component={EditTodo} />
+                    <Route path="/create" component={CreateTodo} />
                 </div>
-            </BrowserRouter>
+            </Router>
         );
     }
 }
-
 export default App;
